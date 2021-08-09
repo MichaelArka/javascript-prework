070da9f9ecc25309c89ba2b2e35f0375dcc25f29
Michal Andronik <michal.androni@gmail.com>
2021-08-09 09:23:55 +0200

    Fixed game results-messages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,66 @@
-let computerMove = `kamień`;
+// Calculating
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if (randomNumber == 2){
+    computerMove = 'papier';
+}
+if (randomNumber == 3){
+    computerMove = 'nożyce';
+}
+// Prompt
+
+printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+if(playerInput == '2'){
+    playerMove = 'papier';
+}
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+// Moves
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+  }
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
